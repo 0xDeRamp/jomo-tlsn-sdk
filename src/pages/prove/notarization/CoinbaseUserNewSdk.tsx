06c3f9a5a505efa8d498ed4cd0846be10846ae16
Charlie Chen <charlie@jomo.network>
2024-07-25 09:43:30 -0700

    Use published extension

--- src/pages/prove/notarization/CoinbaseUserNewSdk.tsx
+++ src/pages/prove/notarization/CoinbaseUserNewSdk.tsx
@@ -6,7 +6,7 @@ import { OpenlayerClientSDK } from 'openlayer-sdk/dist';
 
 function CoinbaseUserNewSdk() {
   const coinbaseServer = "accounts.coinbase.com"
-  const extensionId = "kidmejgglkmmkomccdagfekjfmlgibjb"
+  const extensionId = "bcakokeeafaehcajfkajcpbdkfnoahlh"
   const extensionName = "OpenLayer"
   const redirectUrl = "https://accounts.coinbase.com/profile"
   const urlFilters = ["https://accounts.coinbase.com/api/v1/user"]
